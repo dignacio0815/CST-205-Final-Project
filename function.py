ba9317036c6723e84871d9b1cd17997425f939a2
Denize Ignacio <denizeignacio@gmail.com>
2019-05-12 21:41:30 -0700

    updated translate function

--- function.py
+++ function.py
@@ -3,23 +3,22 @@ from google.cloud import translate
 import pyttsx3
 import pytesseract
 
-
-
 class ImageToText():
     engine = pyttsx3.init()
 
     def getText(self,words):
         text = pytesseract.image_to_string(Image.open(words))
         return text
+
     def speech(text, path):
         text = text.replace('\n', ' ')
         engine.say(text)
         engine.runAndWait()
 
-    def translate(text, target):
-        print(u'Text: {}'.format(result['input']))
-        print(u'Translation: {}'.format(result['translatedText']))
-        print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
-
-
-
+    def translate(self, text, target):
+        translate_client = translate.Client()
+        result = translate_client.translate(text, target_language=target)
+        user_input = result['input']
+        translated_text = result['translatedText']
+        source_language = result['detectedSourceLanguage']
+        return translated_text
